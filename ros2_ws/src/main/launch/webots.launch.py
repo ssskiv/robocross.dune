f69972dc2c67e30b6b32041e44846a49d942145e
Ivan Ioshpa <ivanioshpa@gmail.com>
2025-05-30 02:08:45 +0300

    beta migrate to webots

---
+++ ros2_ws/src/main/launch/webots.launch.py
@@ -0,0 +1,70 @@
+import os
+
+from ament_index_python.packages import get_package_share_directory
+
+import launch
+import pathlib
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription, DeclareLaunchArgument, TimerAction, ExecuteProcess
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.conditions import IfCondition
+from launch.substitutions import LaunchConfiguration, PythonExpression
+
+from launch_ros.actions import Node
+
+from webots_ros2_driver.webots_launcher import WebotsLauncher, Ros2SupervisorLauncher
+from webots_ros2_driver.utils import controller_url_prefix
+from webots_ros2_driver.webots_controller import WebotsController
+
+def generate_launch_description():
+    package_name = 'main'
+    package_dir = get_package_share_directory(package_name)
+    
+    robot_description = os.path.join(package_dir, pathlib.Path(os.path.join(package_dir, 'resource', 'robot.urdf')))
+    
+    default_world = os.path.join(
+        get_package_share_directory(package_name),
+        'worlds',
+        'obstacle.world'
+    )
+    world_arg = DeclareLaunchArgument(
+        'world',
+        default_value = default_world,
+        description = 'World to load'
+    )
+    webots = WebotsLauncher(
+        world=os.path.join(package_dir, 'worlds', 'world.wbt'), ros2_supervisor=True, stream=True
+    )
+    
+    # ros2_supervisor = Ros2SupervisorLauncher()
+
+    # my_robot_driver = Node(
+    #     package='webots_ros2_driver',
+    #     executable='driver',
+    #     output='screen',
+    #     additional_env={'WEBOTS_CONTROLLER_URL': controller_url_prefix() + 'my_robot'},
+    #     parameters=[
+    #         {'robot_description': robot_description},
+    #     ]
+    # )
+    my_robot_driver = WebotsController(
+        robot_name='my_robot',
+        parameters=[
+            {'robot_description': robot_description}
+        ],
+        respawn=True
+    )
+    
+    return LaunchDescription([
+        world_arg,
+        webots,
+        webots._supervisor,
+        # ros2_supervisor,
+        my_robot_driver,
+        launch.actions.RegisterEventHandler(
+            event_handler=launch.event_handlers.OnProcessExit(
+                target_action=webots,
+                on_exit=[launch.actions.EmitEvent(event=launch.events.Shutdown())],
+            )
+        ),
+    ])
